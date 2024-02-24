3fa4d9e0894200a2628e252404ecd2fafba9db54
long0914 <119887736+long0914@users.noreply.github.com>
2024-02-24 22:28:09 +0000

    Defining and implementing an interface

--- Ch02/02_02/BasicInterfaces/Program.cs
+++ Ch02/02_02/BasicInterfaces/Program.cs
@@ -5,20 +5,36 @@ namespace BasicInterfaces
 {
     // TODO: Define an IStorable interface that provides the ability to load and
     // save the information for an object
+    interface IStorable
+    {
+        void Save();
+        void Load();
+        bool NeedsSave { get; set; }
+    }
 
 
     // TODO: Implement IStorable on the Document class
-    class Document
+    class Document : IStorable
     {
         private string name;
 
-        public Document(string s)
-        {
+        public Document(string s) {
             name = s;
             Console.WriteLine("Created a document with name '{0}'", s);
         }
 
-        // TODO: Implement the IStorable interface methods and properties
+        // Implement the IStorable interface methods and properties
+        public void Save() {
+            Console.WriteLine("Saving the document");
+        }
+
+        public void Load() {
+            Console.WriteLine("Loading the document");
+        }
+
+        public bool NeedsSave {
+            get; set;
+        }
     }
 
     class Program
@@ -27,6 +43,10 @@ namespace BasicInterfaces
             Document d = new Document("Test Document");
 
             // TODO: Exercise the IStorable interface
+            // Exercise the IStorable interface
+            d.Load();
+            d.Save();
+            d.NeedsSave = false;
 
             Console.WriteLine("\nPress Enter to continue...");
             Console.ReadLine();
